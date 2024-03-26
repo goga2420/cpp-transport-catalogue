282f0fdadecd787471861eee6fc77fd252394011
goga2420 <58983503+goga2420@users.noreply.github.com>
2024-03-26 17:35:25 +0800

    Изменен main для ввода напрямую

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -20,11 +20,11 @@ int main() {
         }
 
         
-    std::ifstream in("/Users/georgijzukov/Desktop/Transport_Catalogue V2/Transport_Catalogue V2/input.json");
-    std::ofstream out("/Users/georgijzukov/Desktop/Transport_Catalogue V2/Transport_Catalogue V2/out.json");
+//    std::ifstream in("/Users/georgijzukov/Desktop/Transport_Catalogue V2/Transport_Catalogue V2/input.json");
+//    std::ofstream out("/Users/georgijzukov/Desktop/Transport_Catalogue V2/Transport_Catalogue V2/out.json");
     
     catalogue::TransportCatalogue catalogue;
     json_reader::JsonReader catalogue_with_json;
-    catalogue_with_json.BaseRequest(catalogue, in, out);
+    catalogue_with_json.BaseRequest(catalogue, std::cin, std::cout);
 
 }
